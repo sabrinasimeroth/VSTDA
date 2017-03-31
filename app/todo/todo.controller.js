a94f8bb57a0b02059cd888804e43a4715ded6eef
Sabrina Simeroth <Sabrina Simeroth>
2017-03-31 13:32:11 -0700

    updated functions to work with VS controllers and SQL

--- app/todo/todo.controller.js
+++ app/todo/todo.controller.js
@@ -5,25 +5,39 @@
         .module('app')
         .controller('HomeController', HomeController);
 
-    HomeController.$inject = [];
+    HomeController.$inject = ['todoFactory'];
 
     /* @ngInject */
-    function HomeController() {
+    function HomeController(todoFactory) {
       //define variables
       var vm = this;
       vm.list = [];
       vm.priorities = [
-        ["Get'er Done", 3, 'list-group-item list-group-item-danger'],
-        ['You Can Do It', 2, 'list-group-item list-group-item-warning'],
-        ['Fugged About It', 1, 'list-group-item list-group-item-info']
+        {label: "Get'er Done", level:3, style:'list-group-item list-group-item-danger'},
+        {label: 'You Can Do It', level:2, style:'list-group-item list-group-item-warning'},
+        {label: 'Fugged About It',  level:1, style:'list-group-item list-group-item-info'}
       ];
-      vm.count = 0;
 
       //add input to a list upon clicking Add...
       vm.add = function(){
-        vm.list.push({task:vm.input, priority:vm.priority[1], count:vm.count++, style:vm.priority[2]});
-        vm.input = '';
+        todoFactory
+        .create({
+          task:vm.input,
+          priority:vm.priority.level
+        })
+        .then(function(data){
+          vm.list.push(data);
+        });
       }
+
+      vm.getColor = function(todoItem) {
+        switch(todoItem.priority) {
+          case 1: return 'list-group-item list-group-item-info'
+          case 2: return 'list-group-item list-group-item-warning'
+          case 3: return 'list-group-item list-group-item-danger'
+        }
+      }
+
       //order by highest priority
       vm.highPri = function(){
         vm.sort = "-priority";
@@ -38,9 +52,24 @@
       }
 
       //delete based on count
-      vm.remove = function(todoItem){
-        var index = vm.list.indexOf(todoItem);
-        vm.list.splice(index, 1)
+      vm.remove = function (todoItem){
+        todoFactory
+          .remove(todoItem.todoItemId)
+          .then(function(){
+            var index = vm.list.indexOf(todoItem);
+            vm.list.splice(index, 1);
+          });
       }
+
+      activate();
+
+      function activate(){
+        todoFactory
+          .getAll()
+          .then(function(data){
+            vm.list = data;
+          });
+      }
+
     }
 })();
